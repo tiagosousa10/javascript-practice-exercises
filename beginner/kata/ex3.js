da2bf2b8ff77d31d49a1da71aebbc59e2b6707fe
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-23 20:34:01 +0100

    ex3 updated

--- beginner/kata/ex3.js
+++ beginner/kata/ex3.js
@@ -1,10 +1,9 @@
 // total amount of points
 
 const points = (games) => {
-  return games.reduce((acc, game) => {
+  games.reduce((acc, game) => {
     const [x,y] = game.split(':') // x = 1, y = 2
     const points = x > y ? 3 : x === y ? 1 : 0;
-    acc += points;
-    return acc
-  })
+    return acc += points;  
+  },0)
 }
